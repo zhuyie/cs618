62522cdcacde5addb130358abf6e7f7f61939de6
Zhuyie <zhuyie@gmail.com>
2025-11-22 12:00:40 +0000

    feat: add homepage title

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -17,6 +17,7 @@ export function Blog() {
   const posts = postsQuery.data ?? []
   return (
     <div style={{ padding: 8 }}>
+      <h1>Welcome to My Blog!</h1>
       <CreatePost />
       <br />
       <hr />
