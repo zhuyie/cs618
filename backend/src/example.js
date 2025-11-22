37c5bd11b8e3b0b9e0de842a0c927d50dceb7319
Zhuyie <zhuyie@gmail.com>
2025-11-22 01:25:23 +0000

    feat: frontend integrated

--- backend/src/example.js
+++ backend/src/example.js
@@ -1,18 +1,17 @@
 import { initDatabase } from './db/init.js'
 import { Post } from './db/models/post.js'
+import dotenv from 'dotenv'
+dotenv.config()
 
 await initDatabase()
 
 const post = new Post({
-  title: 'Hello Mongoose!',
-  author: 'Huseyin Ergin',
+  title: 'Hello from the outside!',
+  author: 'Some Person',
   contents: 'This post is stored in a MongoDB database using Mongoose.',
-  tags: ['mongoose', 'mongodb'],
+  tags: ['other'],
 })
-const createdPost = await post.save()
-await Post.findByIdAndUpdate(createdPost._id, {
-  $set: { title: 'Hello again, Mongoose!' },
-})
+await post.save()
 
 const posts = await Post.find()
 console.log(posts)
